34728cdeeaacbd7b8d9088c60540f302dc5799db
tatidan <t.danyliuk@gmail.com>
2021-07-06 20:05:39 +0300

    workflow added

--- src/index.js
+++ src/index.js
@@ -1,66 +1,95 @@
 import './sass/main.scss';
 import debounce from 'lodash.debounce';
 import galleryCard from './templates/galleryCard.hbs';
-import ApiService from "./apiService";
+import ApiService from './apiService';
 import { alert, defaultModules } from '@pnotify/core';
 import '@pnotify/core/dist/PNotify.css';
 import '@pnotify/core/dist/BrightTheme.css';
 import * as PNotifyMobile from '@pnotify/mobile';
 import '@pnotify/mobile/dist/PNotifyMobile.css';
-import * as basicLightbox from 'basiclightbox'
+import BtnService from './btn-service';
+import * as basicLightbox from 'basiclightbox';
+import 'basiclightbox/dist/basicLightbox.min.css';
 
 defaultModules.set(PNotifyMobile, {});
 
 const refs = {
-  searchForm: document.querySelector("#search-form"),
-  input: document.querySelector(".input"),
-  response: document.querySelector(".response"),
-  gallery: document.querySelector(".gallery"),
-  //loadMoreBtn: document.querySelector("#load-more"),
+  searchForm: document.querySelector('#search-form'),
+  input: document.querySelector('.input'),
+  submitBtn: document.querySelector('.submitBtn'),
+  response: document.querySelector('.response'),
+  gallery: document.querySelector('.gallery'),
+  anchor: document.querySelector('.anchor'),
 };
 
 const apiService = new ApiService();
 
 refs.searchForm.addEventListener(
-    'input',
+  'input',
   debounce(() => {
     if (!refs.input.value) {
       refs.gallery.innerHTML = '';
       return;
-    }   
+    }
 
     const searchQuery = `${refs.input.value}`;
-    console.log(searchQuery);
-
-
     apiService.searchQuery = refs.input.value;
-    console.log(apiService);
     apiService.resetPage();
+    newLoadMoreBtn.hide();
     clearPage();
 
-    apiService.fetchGalleryCards(searchQuery)
-      .then((cards) => {
+    apiService
+      .fetchGalleryCards(searchQuery)
+      .then(cards => {
         if (!cards.length) {
           throw new Error();
-         }
+        }
         renderGallery(cards);
-      }
-        ).catch(errorHandler);
+      })
+      .catch(errorHandler);
   }, 500),
-)
+);
 
-//тут лежит массив с объектами - 12шт 1стр
+// refs.submitBtn.addEventListener('submit', onSubmit);
 
-function renderGallery(cards) {  
+// function onSubmit(e) {
+//   e.preventDefault();
+
+//   if (!refs.input.value) {
+//     refs.gallery.innerHTML = '';
+//     return;
+//   }
+
+//   const searchQuery = `${refs.input.value}`;
+//   apiService.searchQuery = refs.input.value;
+//   apiService.resetPage();
+//   newLoadMoreBtn.hide();
+//   clearPage();
+
+//   apiService
+//     .fetchGalleryCards(searchQuery)
+//     .then(cards => {
+//       if (!cards.length) {
+//         throw new Error();
+//       }
+//       renderGallery(cards);
+//     })
+//     .catch(errorHandler);
+// }
+
+function renderGallery(cards) {
   refs.gallery.insertAdjacentHTML('beforeend', galleryCard(cards));
+
+  newLoadMoreBtn.show();
+  newLoadMoreBtn.enable();
+  handleButtonClick();
 }
 
-//с ошибками не работает
 function errorHandler() {
-  refs.gallery.innerHTML = "что-то пошло не так";
-  
+  newLoadMoreBtn.hide();
   alert({
-  text: 'Что-то пошло не так. Введите запрос повторно.'
+    text: 'Bad request. Try again.',
+    delay: 500,
   });
   clearPage();
 }
@@ -69,55 +98,49 @@ function clearPage() {
   refs.gallery.innerHTML = '';
 }
 
+const newLoadMoreBtn = new BtnService({
+  selector: "[data-action='load-more']",
+  text: 'LOAD MORE',
+  hidden: true,
+});
 
+newLoadMoreBtn.refs.button.addEventListener('click', onLoadMore);
 
-// const newLoadMoreBtn = new BtnService({
-//   selector: "[data-action='load-more']",
-//   text: "Load More",
-//   hidden: true,
-// });
-// const newShowMoviesBtn = new BtnService({
-//   selector: "[data-action='show-movies']",
-//   text: "Show Movies",
-// });
+function onLoadMore(e) {
+  newLoadMoreBtn.disable();
+  apiService.incrementPage();
+  apiService.fetchGalleryCards().then(renderGallery).catch(errorHandler);
+}
 
-
-// newLoadMoreBtn.refs.button.addEventListener("click", onLoadMore);
-// newShowMoviesBtn.refs.button.addEventListener("click", onShowMovies);
-
-
-//При нажатии на кнопку Load more должна догружаться 
-//следующая порция изображений и рендериться вместе 
-//с предыдущими.
-
-//Страница должна автоматически плавно проскроливаться 
-//после рендера изображений, чтобы перевести пользователя 
-//на следующие загруженные изображения.Используй метод 
+//Страница должна автоматически плавно проскроливаться
+//после рендера изображений, чтобы перевести пользователя
+//на следующие загруженные изображения.Используй метод
 //Element.scrollIntoView().
 
-// const element = document.getElementById('.my-element-selector');
-// element.scrollIntoView({
-//   behavior: 'smooth',
-//   block: 'end',
-// });
+//думаю, что функция пока не работает
+function handleButtonClick() {
+  refs.anchor.scrollIntoView({ block: 'center', behavior: 'smooth' });
+}
 
+document.querySelector('.gallery').onclick = e => {
+  console.log(e.currentTarget);
+  if (e.target.nodeName !== 'IMG') {
+    return;
+  }
+  const largeImg = e.target.dataset.src;
 
-//Можно добавить плагин нотификаций, например pnotify, и показывать 
-//нотификации на результат HTTP - запросов
+  basicLightbox
+    .create(
+      `
+		<img width="100vw" height="auto" src="${largeImg}">
+	`,
+    )
+    .show();
+};
 
-//largeImageURL - ссылка на большое изображение.
-//Можно добавить функционал отображения большой версии изображения 
-//через плагин модального окна, например basicLightbox, при клике 
-//на изображение галереи
+//!!!- нужно убрать скролл pnotify-сообщений
 
-//const basicLightbox = require('basiclightbox');
-// const instance = basicLightbox.create(`
-// 	<h1>Dynamic Content</h1>
-// 	<p>You can set the content of the lightbox with JS.</p>
-// `);
-// const instance = basicLightbox.create(
-//   document.querySelector('.gallery')
-// );
+//!!! - к largeImg можно добавить возврат по escape
 
-//Вместо кнопки Load more можно сделать бесконечную загрузку 
-//при скроле используя Intersection Observer.
+//Вместо кнопки Load more можно сделать бесконечную загрузку
+//при скроле используя Intersection Observer.====== не реализовано
